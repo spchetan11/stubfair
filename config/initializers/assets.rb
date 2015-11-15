d60885a122cbee2432eb202273556538daa01b10
spchetan11 <s.p.chetan11@gmail.com>
2015-11-16 01:40:04 +0530

    stop making stupid spelling mistakes vikram

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( themed/jquery-1.11.2.min.js )
 Rails.application.config.assets.precompile += %w( minimal_form/classie.js )
-Rails.application.config.assets.precompile += %w( minimal_form/stepForm.js )
+Rails.application.config.assets.precompile += %w( minimal_form/stepsForm.js )
 Rails.application.config.assets.precompile += %w( minimal_form/modernizr.custom.js )
 
 # Add additional assets to the asset load path
