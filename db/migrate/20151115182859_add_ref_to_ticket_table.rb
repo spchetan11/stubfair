5eb595696babba2b656e0e22f1f76bd14b775c4f
spchetan11 <s.p.chetan11@gmail.com>
2015-11-16 02:45:21 +0530

    changed cart index events_id

--- db/migrate/20151115182859_add_ref_to_ticket_table.rb
+++ db/migrate/20151115182859_add_ref_to_ticket_table.rb
@@ -1,4 +1,4 @@
-class AddRefToTicketTable < ActiveRecord::Migration
+class AddRkefToTicketTable < ActiveRecord::Migration
   def change
   	add_reference :tickets, :event, references: :events, index: true
     add_foreign_key :tickets, :events, column: :event_id
