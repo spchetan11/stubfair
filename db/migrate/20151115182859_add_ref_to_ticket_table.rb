e0ad402ef42dee18950f7bd19b35efaac1354624
Chetan <s.p.chetan11@gmail.com>
2015-11-19 15:40:18 +0530

    changed migration file class name

--- db/migrate/20151115182859_add_ref_to_ticket_table.rb
+++ db/migrate/20151115182859_add_ref_to_ticket_table.rb
@@ -1,4 +1,4 @@
-class AddRkefToTicketTable < ActiveRecord::Migration
+class AddRefToTicketTable < ActiveRecord::Migration
   def change
   	add_reference :tickets, :event, references: :events, index: true
     add_foreign_key :tickets, :events, column: :event_id
