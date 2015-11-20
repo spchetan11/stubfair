a4e81a681956c39af064203bd90e8bebdf9513f9
Vikram Haridas <vikram.haridas89@gmail.com>
2015-11-20 23:27:52 +0530

    replaced cloudfront with s3 link

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,7 +4,7 @@ require 'paperclip/media_type_spoof_detector'
 
 Paperclip::Railtie.insert
 
-Paperclip::Attachment.default_options[:url] = 'd103hpfao1jklo.cloudfront.net'
+Paperclip::Attachment.default_options[:url] = 'pavan-events.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_region] = 'us-west-2'
