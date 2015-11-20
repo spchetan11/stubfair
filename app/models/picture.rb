b339ba2b31896f7b6c3ff57fa162b46f202ae209
Vikram Haridas <vikram.haridas89@gmail.com>
2015-11-21 02:46:12 +0530

    changing file path names for uploads

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,8 @@ class Picture < ActiveRecord::Base
 	belongs_to :event, class_name: "Event"
 
 	has_attached_file :image,
-    :path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
+    :path => ":rails_root/public/images/:event_id/:filename",
+    :url  => "/images/:event_id/:filename"
 
     do_not_validate_attachment_file_type :image
 end
