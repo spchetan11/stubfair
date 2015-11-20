a4db850baea20fed40a710d90f18a9d38dcb5f5e
Vikram Haridas <vikram.haridas89@gmail.com>
2015-11-20 23:08:32 +0530

    added region to paperclip.rb as well

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -7,6 +7,7 @@ Paperclip::Railtie.insert
 Paperclip::Attachment.default_options[:url] = ':d103hpfao1jklo.cloudfront.net'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_region] = 'us-west-2'
 module Paperclip
   class MediaTypeSpoofDetector
     def spoofed?
