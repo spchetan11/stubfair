52a9d993c4557e900c42a9057021062d66d985c4
Vikram Haridas <vikram.haridas89@gmail.com>
2015-11-27 10:07:42 +0530

    changes for path of picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@ class Picture < ActiveRecord::Base
 	belongs_to :event, class_name: "Event"
 
 	has_attached_file :image,
-    :path => ":rails_root/public/images/:event_id/:filename",
+    :path => ":rails_root/public/images/:id/:filename",
     :url  => "/images/:id/:filename"
 
     do_not_validate_attachment_file_type :image
