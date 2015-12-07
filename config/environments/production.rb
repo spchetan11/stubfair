5d92c6ceda0d99da1b8285cc3059299e1a411a64
spchetan11 <s.p.chetan11@gmail.com>
2015-12-07 19:10:41 +0530

    changed pro.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   :s3_host_name => 's3-us-west-2.amazonaws.com',
   :s3_region => 'us-west-2',
   :bucket => ENV['S3_BUCKET_NAME'],
-  :s3 = AWS::S3.new(
+  :s3 => AWS::S3.new(
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
