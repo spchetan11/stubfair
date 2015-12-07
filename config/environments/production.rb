c898fb94b9dba3dadf2ff2d1e37b4aa73f69436c
spchetan11 <s.p.chetan11@gmail.com>
2015-12-07 19:05:23 +0530

    s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,14 +85,15 @@ Rails.application.configure do
   #enter the amazon s3 credentials here 
   config.paperclip_defaults = {
   :storage => :s3,
-  #:s3_host_name => ENV['s3-us-west-2.amazonaws.com'],
-  :s3_region => ENV['us-west-2'],
+  :s3_host_name => 's3-us-west-2.amazonaws.com',
+  :s3_region => 'us-west-2',
   :bucket => ENV['S3_BUCKET_NAME'],
-  :s3_credentials => {
+  :s3 = AWS::S3.new(
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+    :s3_endpoint => 's3-eu-west-1.amazonaws.com'
+  )
 }
 
   
