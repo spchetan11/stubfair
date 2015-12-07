8fbfaae9039c58c4db3a2bae3edfbc1c7c8c4e50
spchetan11 <s.p.chetan11@gmail.com>
2015-12-07 18:50:12 +0530

    changed production.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -6,8 +6,8 @@ Paperclip::Railtie.insert
 
 #Paperclip::Attachment.default_options[:url] = 'pavan-events.s3.amazonaws.com'
 #Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
-Paperclip::Attachment.default_options[:s3_region] = 'us-west-2'
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+#Paperclip::Attachment.default_options[:s3_region] = 'us-west-2'
 module Paperclip
   class MediaTypeSpoofDetector
     def spoofed?
