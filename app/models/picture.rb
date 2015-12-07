51642c720a55ea1ad9c7741c6c6cd15a0aeef75e
spchetan11 <s.p.chetan11@gmail.com>
2015-12-07 18:11:46 +0530

    checking error in model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,19 +1,9 @@
 class Picture < ActiveRecord::Base
 	belongs_to :event, class_name: "Event"
-
-	has_attached_file :image,
+    has_attached_file :image
+    do_not_validate_attachment_file_type :image
     # :path => ":rails_root/public/images/:id/:filename",
     # :url  => "/images/:id/:filename"
 
-    # :storage => :s3,
-    # :s3_host_name => ENV['s3-us-west-2.amazonaws.com'],
-    # :s3_region => ENV['s3-us-west-2.amazonaws.com'],
-    # :s3_protocol    => "https",
-    # :bucket => ENV['S3_BUCKET_NAME'],
-    # :s3_credentials => {
-    # :bucket => ENV['S3_BUCKET_NAME'],
-    # :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    # :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
-
-    do_not_validate_attachment_file_type :image
+    
 end
