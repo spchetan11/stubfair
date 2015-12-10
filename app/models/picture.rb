704f1dc447ac17d00bbe7e3fa75a764c18a878bb
Chetan <s.p.chetan11@gmail.com>
2015-12-10 17:41:17 +0530

    fxd pics

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,13 +1,11 @@
 class Picture < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+
+ 
 	belongs_to :event, class_name: "Event"
     has_attached_file :image
     do_not_validate_attachment_file_type :image
     # :path => ":rails_root/public/images/:id/:filename",
     # :url  => "/images/:id/:filename"
-
+    
     
 end
