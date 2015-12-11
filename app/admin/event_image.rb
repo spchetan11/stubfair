d72033d17c631e6cb1448bb2f3473c142f6c957b
spchetan11 <s.p.chetan11@gmail.com>
2015-12-11 18:05:53 +0530

    some changes

--- app/admin/event_image.rb
+++ app/admin/event_image.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register EventImage do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+  permit_params :attachment,:id, :image_url
 #
 # or
 #
