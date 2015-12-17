17c21247170f66dd4d08f6b2d96d0758f6d14dbd
spchetan11 <s.p.chetan11@gmail.com>
2015-12-17 19:38:02 +0530

    removed tls

--- config/environment.rb
+++ config/environment.rb
@@ -4,30 +4,33 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-#devise mail setup
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-#    :tls => true,
-#    :address => "smtp.gmail.com",
-#    :port => 587,
-#    :domain => "gmail.com",
-#    :authentication => :login,
-#    :user_name => "[username]",
-#    :password => "[password]"
-#  }
-require 'tlsmail'    
-Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 
+#devise mail setup
+
+#require 'tlsmail'  
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,  
-  :address            => 'smtp.gmail.com',
-  :port               => 587,
-  :tls                  => true,
-  :domain             => 'gmail.com', #you can also use google.com
-  :authentication     => :plain,
-  :user_name          => 'stubfairsmtp@gmail.com',
-  :password           => 'stubfair1'
-}
+   :tls => true,
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => :plain,
+   :user_name => "stubfairsmtp@gmail.com",
+   :password => "stubfair1"
+ }
+# require 'tlsmail'    
+# Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.raise_delivery_errors = true
+# ActionMailer::Base.smtp_settings = {
+#   :enable_starttls_auto => true,  
+#   :address            => 'smtp.gmail.com',
+#   :port               => 587,
+#   :tls                  => true,
+#   :domain             => 'gmail.com', #you can also use google.com
+#   :authentication     => :plain,
+#   :user_name          => 'stubfairsmtp@gmail.com',
+#   :password           => 'stubfair1'
+# }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,8 +102,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'stubfair.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :address => "127.0.0.1",
-  :port    => 25,
+  :address => "smtp.gmail.com",
+  :port    => 587,
   :domain  => 'stubfair.herokuapp.com'
 }
 end
