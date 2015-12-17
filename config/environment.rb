1f7d8f1f8149b1b2d6197b43b839b9a2254d6f81
spchetan11 <s.p.chetan11@gmail.com>
2015-12-17 20:34:51 +0530

    tired

--- config/environment.rb
+++ config/environment.rb
@@ -19,19 +19,20 @@ Rails.application.initialize!
 #    :user_name => "stubfairsmtp@gmail.com",
 #    :password => "stubfair1"
 #  }
-# require 'tlsmail'    
-# Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+  require 'tlsmail'    
+ Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.perform_deliveries = true
-# ActionMailer::Base.raise_delivery_errors = true
-# ActionMailer::Base.smtp_settings = {
-#   :enable_starttls_auto => true,  
-#   :address            => 'smtp.gmail.com',
-#   :port               => 587,
-#   :tls                  => true,
-#   :domain             => 'gmail.com', #you can also use google.com
-#   :authentication     => :plain,
-#   :user_name          => 'stubfairsmtp@gmail.com',
-#   :password           => 'stubfair1'
-# }
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,  
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :tls                  => true,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => 'stubfairsmtp@gmail.com',
+  :password           => 'stubfair1',
+  :openssl_verify_mode  => 'none'
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
 
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  
+
   #devise mailer
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
