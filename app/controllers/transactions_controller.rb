37b7463d97c32e3b757435cca8de61c09db6f3e5
Sreenath B <bsreeinf@gmail.com>
2016-01-09 05:44:05 +0530

    commented out email

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -58,7 +58,7 @@ class TransactionsController < ApplicationController
         @user_transaction = @transaction.update(status: status, transaction_id: txn_id, purchased_at: Time.now, purchase_amount: payment_gross, number_of_tickets_purchased: quant, :purchased => true)
         #session[:cart] = nil
         UserMailer.tickets_purchased(current_user).deliver_now
-        UserMailer.tickets_sold(seller).deliver_now
+        #UserMailer.tickets_sold(seller).deliver_now
     else
         render nothing: true
     end
