7b28f52ead0e631e2c1824efc41cf4254482f46d
spchetan11 <s.p.chetan11@gmail.com>
2016-02-04 17:23:07 +0530

    added paypal email migration and edited the pdf view a bit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201143720) do
+ActiveRecord::Schema.define(version: 20160204114144) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -222,6 +222,7 @@ ActiveRecord::Schema.define(version: 20160201143720) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "paypal_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
